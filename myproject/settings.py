51bb910f97d94c1ac529ccf4f69f9ccabfaee45f
Alifiya Alema <alfiyaalema666@gmail.com>
2026-07-13 13:54:34 +0530

    Prepare project for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u%5+xb0&g+#gdn)-ph28r==lo&^5e-%-8zf8ihngt862-iatiy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
